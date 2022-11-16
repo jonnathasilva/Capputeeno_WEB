ea52fcf5ce5bca46f5cb218005c46cea3e179778
jonnathan silva <sjonnathan928@gmail.com>
2022-11-16 15:26:32 -0300

    mock react-router-dom

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -2,19 +2,16 @@ import { describe, expect, it, vi } from "vitest";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
+import { MemoryRouter } from "react-router-dom";
 
 import { Header } from "./index";
-
-vi.mock("react-router-dom", () => ({
-  ...vi.importActual("react-router-dom"),
-  useNavigate: () => vi.fn(),
-  Link: (params: any) => <div {...params} />,
-}));
 
 describe("Component Header", () => {
   it("should show cart value", () => {
     const { getByTestId } = render(
-      <Header cartLength={1} setCurrentPage={vi.fn} />
+      <MemoryRouter initialEntries={["/"]}>
+        <Header cartLength={1} setCurrentPage={vi.fn} />
+      </MemoryRouter>
     );
 
     expect(getByTestId("cartVelue")).toBeInTheDocument();
@@ -22,23 +19,29 @@ describe("Component Header", () => {
 
   it("should not show cart value", () => {
     const { queryByTestId } = render(
-      <Header cartLength={0} setCurrentPage={vi.fn} />
+      <MemoryRouter initialEntries={["/"]}>
+        <Header cartLength={0} setCurrentPage={vi.fn} />
+      </MemoryRouter>
     );
 
     expect(queryByTestId("cartVelue")).not.toBeInTheDocument();
   });
 
   it("should redirect to cart page", () => {
-    const { getByTestId } = render(
-      <Header cartLength={0} setCurrentPage={vi.fn} />
+    const { getByRole } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <Header cartLength={0} setCurrentPage={vi.fn} />
+      </MemoryRouter>
     );
 
-    expect(getByTestId("Link")).toHaveAttribute("to", "/cart");
+    expect(getByRole("link")).toHaveAttribute("href", "/cart");
   });
 
   it("should show modal", async () => {
     const { getByTestId } = render(
-      <Header cartLength={0} setCurrentPage={vi.fn} />
+      <MemoryRouter initialEntries={["/"]}>
+        <Header cartLength={0} setCurrentPage={vi.fn} />
+      </MemoryRouter>
     );
 
     await userEvent.click(getByTestId("isModal"));
@@ -48,7 +51,9 @@ describe("Component Header", () => {
 
   it("should not show modal", () => {
     const { getByTestId } = render(
-      <Header cartLength={0} setCurrentPage={vi.fn} />
+      <MemoryRouter initialEntries={["/"]}>
+        <Header cartLength={0} setCurrentPage={vi.fn} />
+      </MemoryRouter>
     );
 
     expect(getByTestId("modal")).toHaveClass("hidden");
