ecd0e78683ba9139a7407bb6a4f815166797504f
jonnathan silva <sjonnathan928@gmail.com>
2022-12-12 16:59:07 -0300

    refactoring products

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -19,20 +19,20 @@ export const Pagination: React.FC<Product> = ({ currentItens, page }) => {
     <section className="flex flex-col space-y-6">
       {page > 1 ? <ButtonPagination page={page} /> : ""}
 
-      <div className="flex flex-wrap">
+      <div className="grid grid-cols-1 flex-wrap gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {currentItens?.map((product) => (
-          <div
-            key={product._id}
-            className=" h-96 px-0 w-full sm:w-[calc(100%/2)] sm:px-2 md:w-[calc(100%/3)] lg:px-4 lg:w-[calc(100%/4)]  "
-          >
-            <Link to={`/product/${product._id}`} className="cursor-pointer ">
+          <div key={product._id} className="h-96 px-0 w-full shadow-lg">
+            <Link
+              to={`/product/${product._id}`}
+              className="cursor-pointer h-96 block"
+            >
               <img
                 src={product.img}
                 alt={product.title}
                 className="w-full h-72 rounded-t-lg object-cover"
               />
 
-              <div className="bg-white-500 px-3 py-2 text-black-400 font-light">
+              <div className="bg-white-500 px-3 py-2 text-black-400 font-light h-24">
                 <p className="border-b border-gray-100 pb-2">{product.title}</p>
 
                 <p className="pt-2 font-bold to-black-500">
